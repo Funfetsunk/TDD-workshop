2e612a0d8586482184e83ef78cb666b2df6e4f09
Emily Sas <emily.sas@comparethemarket.com>
2016-09-22 22:12:56 +0100

    Adding files for RomanNumerals kata

---
+++ Code/AutomaticUmbrella.cs
@@ -0,0 +1,24 @@
+﻿namespace Code
+{
+    public class AutomaticUmbrella
+    {
+        public IWetWeather WetWeather { get; set; }
+
+        public bool IsOpen {
+            get { return WetWeather.IsOccuring(); }
+            private set {}
+        }
+
+        public  AutomaticUmbrella()
+        {
+            IsOpen = false;
+        }
+    }
+
+
+    // this is here for the sake of the exercise but should sit in its own file
+    public interface IWetWeather
+    {
+        bool IsOccuring();
+    }
+}
---
+++ Tests/AutomaticUmbrellaTests.cs
@@ -0,0 +1,38 @@
+﻿using Code;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Tests
+{
+    class AutomaticUmbrellaTests
+    {
+        private IWetWeather _mockRain;
+        private AutomaticUmbrella _umbrella;
+
+        [SetUp]
+        public void Init()
+        {
+            _mockRain = MockRepository.GenerateMock<IWetWeather>();
+            _umbrella = new AutomaticUmbrella {WetWeather = _mockRain};
+
+            // create a mockHail then uncomment the line below
+            //_sturdyUmbrella = new AutomaticUmbrella {WetWeather = _mockHail};
+        }
+
+        [Test]
+        public void Should_OpenWhenWeatherIsWet()
+        {
+            _mockRain.Stub(x => x.IsOccuring()).Return(true);
+            Assert.That(_umbrella.IsOpen, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Should_CloseWhenWetherIsNotWet()
+        {
+           // stub the rain so that when the umbrella it recieves the message IsOccuring it returns false
+           // assert that the umbrella is not open (is open equals false)
+        }
+
+        // Write a test that tests that the sturdy umbrella opens when it's hailing
+    }
+}
