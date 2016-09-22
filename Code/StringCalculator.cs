ca6c721c908e677b7ca01bca8b443363b4e89ac0
Emily Sas <emily.sas@comparethemarket.com>
2016-09-22 22:04:24 +0100

    Adding files to demo mocking

--- Code/StringCalculator.cs
+++ Code/StringCalculator.cs
@@ -6,6 +6,10 @@ namespace Code
     {
     // write a method Add that takes two strings num1 and num2 and returns
     // the sum of the two numbers
+        public string Add(string num1, string num2)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
 }
--- Tests/StringCalculatorTests.cs
+++ Tests/StringCalculatorTests.cs
@@ -7,5 +7,18 @@ namespace Tests
     public class StringCalculatorTests
     {
         // write tests here for the StringCalculator
+
+        [Test]
+        public void Should_AddOneAndTwoCorrectly()
+        {
+            // Assert.That(stringCalc.Add("1", "2"), Is.EqualTo("3"));
+            // Assert.True(stringCalc.Add("1", "2") == 3);
+        }
+
+        //Write a second test here, as above (inc [Test])
+
+            // Assert.That(stringCalc.Add("1", "3"), Is.EqualTo("4"));
+            // Assert.True(stringCalc.Add("1", "3") == 4);
+
     } 
 }
